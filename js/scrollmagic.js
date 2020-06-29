c8439bb4e1fa7c0f06b1b8521a9c782d12f3d3f8
MrGorlenko <52908127+MrGorlenko@users.noreply.github.com>
2020-06-29 12:10:52 +0300

    third section and animation of the nav bar

--- js/scrollmagic.js
+++ js/scrollmagic.js
@@ -2,10 +2,36 @@ $(document).ready(function(){
 
     $(function(){
         $(document).scroll(function(){
-            if($(this).scrollTop() >= $('.world').offset().top - 100) {
+            if($(this).scrollTop() >= $('.world').offset().top - 500) {
                 $(".tummo").addClass('act')
             } else {
                 $(".tummo").removeClass('act')
+            }
+        });
+    });
+
+    $(function(){
+        $(document).scroll(function(){
+            if($(this).scrollTop() >= $('#art').offset().top-500) {
+                $(".art-link").addClass('act')
+                $('#mainNav').addClass('line-hidden')
+            } else {
+                $(".art-link").removeClass('act')
+                $('#mainNav').removeClass('line-hidden')
+            }
+        });
+    });
+
+    $(function(){
+        $(document).scroll(function(){
+            if($(this).scrollTop() >= $('#silk').offset().top-1300) {
+                // $(".art-link").addClass('act')
+                $('#mainNav').removeClass('line-hidden')
+                // alert('yo')
+                console.log('yo')
+            } else {
+                // $(".art-link").removeClass('act')
+                // $('#mainNav').removeClass('line-hidden')
             }
         });
     });
