c3ad5e1dd6b4c4e8e07bb66828851ed09910e969
MrGorlenko <52908127+MrGorlenko@users.noreply.github.com>
2020-07-01 20:53:30 +0300

    switcher

--- js/item.js
+++ js/item.js
@@ -1,9 +1,37 @@
 $(document).ready(function(){
-    $('.burger').click(function(){
-        $('.main-menu').removeClass('menu-none')
+
+    $('.accessory').click(function(){
+        $('.accessory').addClass('act')
+        $('.jewerly').removeClass('act')
+
+        $('.item-2-2').removeClass('item-none')
+        $('.item-1-2').removeClass('item-none')
+
+        $('.item-2-1').addClass('item-none')
+        $('.item-1-1').addClass('item-none')
+
+
+        // $('.item-1-2').addClass('item-none')
+        // $('.item-2-2').addClass('item-none')
+
+        // $('.item-1-1').removeClass('item-none')
+        // $('.item-2-1').removeClass('item-none')
     })
 
-    $('.close').click(function(){
-        $('.main-menu').addClass('menu-none')
+    $('.jewerly').click(function(){
+        $('.jewerly').addClass('act')
+        $('.accessory').removeClass('act')
+
+        $('.item-2-1').removeClass('item-none')
+        $('.item-1-1').removeClass('item-none')
+
+        $('.item-2-2').addClass('item-none')
+        $('.item-1-2').addClass('item-none')
+
+        // $('.item-1-2').addClass('item-none')
+        // $('.item-2-2').addClass('item-none')
+
+        // $('.item-1-1').addClass('item-none')
+        // $('.item-1-2').addClass('item-none')
     })
 })
