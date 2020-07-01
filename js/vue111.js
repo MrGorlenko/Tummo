ed808900d940b6da21ae37944bea77a4196359ae
MrGorlenko <52908127+MrGorlenko@users.noreply.github.com>
2020-07-01 14:25:11 +0300

    basket realization

--- js/vue111.js
+++ js/vue111.js
@@ -2,18 +2,23 @@ var arr = new Vue({
 
     el: '#app',
     data:{
+        amount:0,
+        price:2400,
         count:1,
+        total: 0,
         modalState: 'modal-passive'
     },
     methods:{
         addItem: function(){
             this.count ++
+            this.price += 2400
         },
         removeItem: function(e){
 
             switch(this.count>1){
                 case true:
                     this.count--;
+                    this.price -= 2400
                 case false:
                     e.preventDefault();
                     break;
@@ -24,7 +29,12 @@ var arr = new Vue({
         },
         callModal: function(){
             this.modalState = 'modal-active'
+        },
+        buyItem: function() {
+            this.amount += this.count
+            this.total += this.price
         }
+
     }
 
 })
